a091eb66002e5ea864aeb7e95418a71da2eb398d
Harry Merritt <harry@merritt.es>
2020-07-23 02:16:29 +0100

    setup key functions to game-of-life

---
+++ app/src/utils/game-of-life.js
@@ -0,0 +1,10 @@
+
+/*
+ * Calculate state of next generation
+ */
+export const nextGeneration = (matrix) => {}
+
+/*
+ * Get number of living neighbours of a specific cell in matrix
+ */
+export const livingNeighbours = (matrix, cell) => {}
