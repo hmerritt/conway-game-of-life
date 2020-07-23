d217c1e438d903cd43574c7f91a46549ceea3450
Harry Merritt <harry@merritt.es>
2020-07-23 01:54:32 +0100

    render generated matrix as rows and columns in App

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { createMatrix } from "./utils/matrix";
 
 function App() {
+
+    // Grid matrix
+    const [matrix, setMatrix] = useState(createMatrix());
+
+    console.log(createMatrix());
+
     return (
         <div className="App">
             <h1>Game Of Life</h1>
+            {
+                matrix.map((valY, indexY) => {
+                    return [...matrix[indexY].map((valX, indexX) => {
+                        return (
+                            <div className="cell">{matrix[indexY][indexX]} </div>
+                        );
+                    }), <><br /></>]
+                })
+            }
         </div>
     );
 }
