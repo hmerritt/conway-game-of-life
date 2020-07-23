61955eee0e9421483c55dcb07608655f54f0aac7
Harry Merritt <harry@merritt.es>
2020-07-23 17:38:12 +0100

    calculate state of next generation from current matrix

--- app/src/utils/game-of-life.js
+++ app/src/utils/game-of-life.js
@@ -1,11 +1,6 @@
 import { arrayIndexExists } from "./matrix";
 
 /*
- * Return state of next generation
- */
-export const nextGeneration = (matrix) => {}
-
-/*
  * Return number of living neighbours of a specific cell in matrix
  */
 export const livingNeighbours = (matrix, cell = [0, 0]) => {
@@ -48,4 +43,43 @@ export const livingNeighbours = (matrix, cell = [0, 0]) => {
     if ( isRightBottom && matrix[ cell[0] + 1 ][ cell[1] + 1 ] ) count++;
 
     return count;
+}
+
+/*
+ * Return state of next generation
+ */
+export const nextGeneration = (matrix) => {
+    const newMatrix = [];
+
+    for (let stepY = 0; stepY < matrix.length; stepY++) {
+        const arrX = [];
+
+        for (let stepX = 0; stepX < matrix[stepY].length; stepX++) {
+            let cell = matrix[stepY][stepX];
+            let newCell = 0;
+            let cellLivingNeighbours = livingNeighbours(matrix, [stepY, stepX]);
+
+            // Cell is already living
+            if (cell === 1) {
+                // Stay living if cell has 2 or 3 living neighbours
+                if (cellLivingNeighbours === 2 || cellLivingNeighbours === 3) {
+                    newCell = 1;
+                }
+            }
+
+            // Cell is dead
+            else {
+                // Rise from dead if cell has exactly 3 living neighbours
+                if (cellLivingNeighbours === 3) {
+                    newCell = 1;
+                }
+            }
+
+            arrX.push(newCell);
+        }
+
+        newMatrix.push(arrX);
+    }
+
+    return newMatrix;
 }
