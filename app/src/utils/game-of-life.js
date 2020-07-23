7c486eed9f7c9a28545c387df77f476f42f12d77
Harry Merritt <harry@merritt.es>
2020-07-23 17:17:19 +0100

    livingNeighbours returns number of living neighbours of a specific cell

--- app/src/utils/game-of-life.js
+++ app/src/utils/game-of-life.js
@@ -1,10 +1,51 @@
+import { arrayIndexExists } from "./matrix";
 
 /*
- * Calculate state of next generation
+ * Return state of next generation
  */
 export const nextGeneration = (matrix) => {}
 
 /*
- * Get number of living neighbours of a specific cell in matrix
+ * Return number of living neighbours of a specific cell in matrix
  */
-export const livingNeighbours = (matrix, cell) => {}
+export const livingNeighbours = (matrix, cell = [0, 0]) => {
+    let count = 0;
+
+    let isLeft, isRight, isTop, isBottom, isLeftTop, isRightTop, isLeftBottom, isRightBottom;
+
+    isLeft = arrayIndexExists(matrix[cell[0]], cell[1] - 1);
+    isRight = arrayIndexExists(matrix[cell[0]], cell[1] + 1);
+    isTop = arrayIndexExists(matrix, cell[0] - 1);
+    isBottom = arrayIndexExists(matrix, cell[0] + 1);
+
+    isLeftTop = isLeft && isTop;
+    isRightTop = isRight && isTop;
+    isLeftBottom = isLeft && isBottom;
+    isRightBottom = isRight && isBottom;
+
+    // Left
+    if ( isLeft && matrix[ cell[0] ][ cell[1] - 1 ] ) count++;
+
+    // Right
+    if ( isRight && matrix[ cell[0] ][ cell[1] + 1 ] ) count++;
+
+    // Top
+    if ( isTop && matrix[ cell[0] - 1 ][ cell[1] ] ) count++;
+
+    // Bottom
+    if ( isBottom && matrix[ cell[0] + 1 ][ cell[1] ] ) count++;
+
+    // Left Top
+    if ( isLeftTop && matrix[ cell[0] - 1 ][ cell[1] - 1 ] ) count++;
+
+    // Right Top
+    if ( isRightTop && matrix[ cell[0] - 1 ][ cell[1] + 1 ] ) count++;
+
+    // Left Bottom
+    if ( isLeftBottom && matrix[ cell[0] + 1 ][ cell[1] - 1 ] ) count++;
+
+    // Right Bottom
+    if ( isRightBottom && matrix[ cell[0] + 1 ][ cell[1] + 1 ] ) count++;
+
+    return count;
+}
