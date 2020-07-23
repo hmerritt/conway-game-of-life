fac5500d188ee10f8364ecf2420c43da1db38383
Harry Merritt <harry@merritt.es>
2020-07-23 20:35:25 +0100

    use Cell component to render the grid matrix

--- app/src/App.js
+++ app/src/App.js
@@ -1,35 +1,64 @@
 import React, { useState, useEffect } from "react";
 
+import { nextGeneration } from "./utils/game-of-life";
 import { createMatrix, randomBinary } from "./utils/matrix";
+
+import Cell from "./components/Cell";
 
 function App() {
 
     // Grid matrix
     const [matrix, setMatrix] = useState([[]]);
+    const [matrixSize, setMatrixSize] = useState(25);
+
+    //
+    const handleNextGeneration = () => {
+        setMatrix(nextGeneration(matrix))
+    }
+
+    // Toggle cell value
+    const toggleCellValue = (cell) => {
+        const newMatrix = [...matrix];
+        const cellValue = newMatrix[ cell[0] ][ cell[1] ];
+        newMatrix[ cell[0] ][ cell[1] ] = !cellValue ? 1 : 0;
+        setMatrix(newMatrix);
+    }
 
     // Generate matrix and populate with random bits
     useEffect(() => {
-        setMatrix(createMatrix(25, 25, randomBinary));
-    }, []);
-
-    useEffect(() => {
-        console.log(matrix);
-    }, [matrix]);
+        setMatrix(createMatrix(matrixSize, matrixSize, randomBinary));
+    }, [matrixSize]);
 
     return (
-        <div className="App">
-            <h1>Game Of Life</h1>
+        <div className="App" onClick={() => { /*setMatrix(nextGeneration(matrix))*/ }}>
+            <div className="container">
+                <h1>Game Of Life</h1>
+                <h2>{matrixSize} x {matrixSize}</h2>
 
-            <div className="table">
-                {
-                    matrix.map((valY, indexY) => {
-                        return [...matrix[indexY].map((valX, indexX) => {
+                <div className="grid">
+                    {
+                        matrix.map((valY, indexY) => {
                             return (
-                                <div className={`cell${matrix[indexY][indexX] ? " active" : ""}`}></div>
-                            );
-                        }), <><br /></>]
-                    })
-                }
+                                <div className="grid-row" key={indexY}>
+                                    {
+                                        matrix[indexY].map((valX, indexX) => {
+                                            return (
+                                                <Cell
+                                                    cell={[indexY, indexX]}
+                                                    living={matrix[indexY][indexX]}
+                                                    toggleCellValue={toggleCellValue}
+                                                    key={indexY + indexX}
+                                                />
+                                            )
+                                        })
+                                    }
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+
+                <button onClick={handleNextGeneration}>Next State</button>
             </div>
         </div>
     );
